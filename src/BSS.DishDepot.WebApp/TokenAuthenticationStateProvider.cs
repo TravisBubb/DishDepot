0245c4b0def6d432c8bf6210a8c50e4a7c6206e2
TravisBubb <travisbubb14@gmail.com>
2024-09-18 22:25:13 -0400

    WIP: Begin playing around with Blazor as a user-facing web application

---
+++ src/BSS.DishDepot.WebApp/TokenAuthenticationStateProvider.cs
@@ -0,0 +1,39 @@
+﻿using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace BSS.DishDepot.WebApp
+{
+    public class TokenAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ISessionStorageService _sessionStorageService;
+        private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public TokenAuthenticationStateProvider(ISessionStorageService sessionStorageService)
+        {
+            _sessionStorageService = sessionStorageService;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _sessionStorageService.GetItemAsync<string>("access_token");
+            if (string.IsNullOrEmpty(token))
+                return new AuthenticationState(_anonymous);
+
+            var securityToken = new JwtSecurityToken(token);
+            var identity = new ClaimsIdentity(securityToken.Claims);
+            var user = new ClaimsPrincipal(identity);
+            return new AuthenticationState(user);
+        }
+
+        public void AuthenticateUser(string token)
+        {
+            var securityToken = new JwtSecurityToken(token);
+            var identity = new ClaimsIdentity(securityToken.Claims);
+            var user = new ClaimsPrincipal(identity);
+            var state = new AuthenticationState(user);
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+        }
+    }
+}
